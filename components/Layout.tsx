a3a7ea2a8b31086b3d3fbdba788a914388688ea2
kenji <kenjix12x22@gmail.com>
2021-08-24 22:42:17 +0900

    （削除）keigo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     <header>
       <nav>
         <Link href="/">
-          <a>Home(keigo)</a>
+          <a>Home</a>
         </Link>{' '}
         |{' '}
         <Link href="/about">
