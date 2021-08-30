5ebd7b1e7f8501c0f62082786c57720bb7a5722d
kenji <kenjix12x22@gmail.com>
2021-08-31 02:35:50 +0900

    【追記】hello

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
