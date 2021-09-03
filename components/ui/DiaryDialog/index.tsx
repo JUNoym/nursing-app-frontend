b57730412426fc009b4e01947e5c81a9f11f7bbf
kenji <kenjix12x22@gmail.com>
2021-09-03 15:27:06 +0900

    【修正】アロー関数に変更

--- components/ui/DiaryDialog/index.tsx
+++ components/ui/DiaryDialog/index.tsx
@@ -6,7 +6,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-export default function AlertDialog() {
+const DiaryDialog = () => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -48,4 +48,6 @@ export default function AlertDialog() {
       </Dialog>
     </div>
   );
-}
+};
+
+export default DiaryDialog;
