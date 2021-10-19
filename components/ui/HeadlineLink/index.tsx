7b763673036899528092f417d0adce42b5c5560e
JUNoym <junnyaooyama@gmail.com>
2021-10-20 04:05:50 +0900

    ✨ リンク設定

---
+++ components/ui/HeadlineLink/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './index.module.scss'
+import Link from 'next/link'
+
+
+// material-ui
+import Button from '@material-ui/core/Button'
+
+
+// コンポーネント
+
+interface Props {
+}
+
+export const HeadlineLink = (props: Props) => {
+    return (
+        <div className={styles.content}>
+            <div className={styles.link}>
+                <Button classes={{ root: styles.button }}>
+                    <Link href="/excretion">
+                        <a>排泄記録表</a>
+                    </Link>
+                </Button>
+                <Button classes={{ root: styles.button }}>
+                    <Link href="/vital">
+                        <a>バイタルチェック</a>
+                    </Link>
+                </Button>
+                <Button classes={{ root: styles.button }}>
+                    <Link href="/">
+                        <a>夜間日誌</a>
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    )
+}
+
