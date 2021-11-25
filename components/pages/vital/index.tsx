244768b7244e9202924b05c00fed5028d6f332d3
JUNoym <junnyaooyama@gmail.com>
2021-11-26 04:58:21 +0900

    テスト

--- components/pages/vital/index.tsx
+++ components/pages/vital/index.tsx
@@ -30,6 +30,7 @@ const Index = () => {
                         <Grid container spacing={1}>
                             <Grid item xs={12}>
                                 <h1>バイタルチェック</h1>
+                                {/* テスト */}
                             </Grid>
 
                             <Grid item xs={12}>
