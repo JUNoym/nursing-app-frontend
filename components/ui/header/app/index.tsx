ec4e54e24e3a1b2e4642b31ee959cf8d414b27be
JUNoym <junnyaooyama@gmail.com>
2021-12-19 04:41:57 +0900

    検索ページとログイン後のヘッダーを作成

--- components/ui/header/app/index.tsx
+++ components/ui/header/app/index.tsx
@@ -1,11 +1,18 @@
 import styles from './index.module.scss'
 import React from 'react'
+import Link from 'next/link'
+
 
 // material
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+
+// mui
+import SearchIcon from '@material-ui/icons/Search';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
 
 interface Props { }
 
@@ -15,27 +22,26 @@ const Header = (props: Props) => {
         <div className={styles.content}>
             <AppBar position="fixed" classes={{ root: styles.RootBar }}>
                 <Container className={styles.container}>
-                    <Toolbar >
-                        <h1>夜勤日誌アプリ</h1>
+                    <Toolbar className={styles.tool}>
+                        <h3>夜勤日誌アプリ</h3>
                         <div className={styles.link}>
-                            <Button>
-                                排泄
-                                </Button>
-                            <Button>
-                                日誌
-                                </Button>
-                            <Button>
-                                バイタル
-                                </Button>
-                        </div>
-                        <div className={styles.signupWrapper}>
-                            <Button>
-                                Login
+                            <Button variant="outlined">
+                                <Link href="/search">
+                                    <SearchIcon color="primary" fontSize="large" />
+                                </Link>
                             </Button>
-                            <Button variant="outlined" classes={{ root: styles.Mroot }} >
-                                Signup
+                            <Button variant="outlined">
+                                <Link href="/archive">
+                                    <CalendarTodayIcon color="primary" fontSize="large" />
+                                </Link>
+                            </Button>
+                            <Button variant="outlined">
+                                <Link href="/">
+                                    <AccountCircleIcon color="primary" fontSize="large" />
+                                </Link>
                             </Button>
                         </div>
+
                     </Toolbar>
                 </Container>
             </AppBar>
