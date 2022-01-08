3f591daec90a666f9159ee78ad7b5806d0195278
JUNoym <junnyaooyama@gmail.com>
2022-01-08 15:39:39 +0900

    ログインページ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import { AppProps } from 'next/app'
+import React, { useState } from 'react'
+import Layout from "../components/Layout2"
+import Index from '../components/pages/login'
+
 
 // material-ui
 import { CssBaseline } from '@material-ui/core'
@@ -8,9 +12,21 @@ import { ThemeProvider, StylesProvider } from '@material-ui/core/styles'
 import '../styles/global.scss'
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Component {...pageProps} />
-        </>
-    );
+    const [loggedInStatus, setLoggedInStatus] = useState("未ログイン")
+    if (loggedInStatus === "ログイン中") {
+        return (
+            <div>
+                <Component {...pageProps} />
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                < Layout title="夜勤日誌アプリ" >
+                    <CssBaseline />
+                    <Index loggedInStatus={loggedInStatus} setLoggedInStatus={setLoggedInStatus} />
+                </Layout >
+            </div>
+        )
+    }
 }
