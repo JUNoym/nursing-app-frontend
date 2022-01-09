08fe8eb1058ba69faf0fbfa2ff6221d284af7a4e
JUNoym <junnyaooyama@gmail.com>
2022-01-10 06:34:41 +0900

    記録表示

--- components/DisplayName/index.tsx
+++ components/DisplayName/index.tsx
@@ -3,10 +3,16 @@ import styles from './index.module.scss'
 
 
 const index = (props) => {
-    const { user_name } = props
+    const { user_name, info } = props
+    console.log(info, 'info')
     return (
         <div className={styles.content}>
             <p>{user_name}</p>
+            {/* {info ? Object.entries(info).map((item, index) => {
+                return (
+                    <p>{item}</p>
+                )
+            }) : <p>まだ投稿はありません</p>} */}
         </div>
     )
 }
--- components/pages/result/index.tsx
+++ components/pages/result/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from './index.module.scss'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
 
 // material-ui
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -22,26 +23,29 @@ type Array = {
 const index = () => {
     const router = useRouter()
     const [data, setData] = useState<Array>()
-    const [name, setName] = useState()
-    const [info, setInfo] = useState()
-
-
+    // const { data, error } = useSWR(`api().get('/search?q=${router.query.q}')`)
 
     useEffect(() => {
         const fetchData = async () => {
             if (router.query.type == 'name') {
                 const res = await api().get(`/archives/search?type=name&q=${router.query.q}`)
-                console.log(res.data, 'res.data')
-                setData(res.data)
-                console.log(data, 'data') // → ここにデータが入ってこないのが問題
-
+                console.log(res.data.result, 'res.data')
+                setData(res.data.result)
+                // {type: 'vital', user_name: 'isomura', info: {…}}
+                var array = Object.entries(data)
+                console.log(array, 'array')
+                setData(array)
             }
             // http://localhost:3030/api/v1/archives/search/?type=date&q=2021-12-27
             else if (router.query.type == 'date') {
                 const res = await api().get(`/archives/search?type=date&q=${router.query.q}`)
                 setData(res.data)
+                var array = Object.entries(data)
+                console.log(array, 'array')
+                setData(array)
 
             }
+
         }
         fetchData()
     }, [])
@@ -54,7 +58,19 @@ const index = () => {
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
                         {
-                            <p>{JSON.stringify(data)}</p>
+                            // <p>{JSON.stringify(data)}</p>
+                            // 三項演算子
+                            data ? data.map((item, index) => {
+                                return (
+                                    <DisplayName
+                                        key={index}
+                                        info={item.info}
+                                        type={item.type}
+                                        user_name={item.user_name}
+                                    />
+                                )
+                            }) : <p>データがありません</p>
+
                         }
                     </Grid>
                 </Grid>
