4fb96e60231f4175b1451bbac1535eb1452fbeb4
JUNoym <junnyaooyama@gmail.com>
2022-03-23 00:42:15 +0900

    トーストテスト実装

--- components/pages/toast/index.tsx
+++ components/pages/toast/index.tsx
@@ -1,46 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 import styles from './index.module.scss'
-import { useForm } from 'react-hook-form'
-
-// material-ui
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Box from '@material-ui/core/Box'
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button'
-import FormControl from '@material-ui/core/FormControl'
-
-// api
-import api from '../../../api/config'
-
-// token
-import { setAuthorizationHeader } from '../../../api/config'
-
-// type
-type Inputs = {
-    name: string
-    email: string
-    password: string
-}
 
 
+const notify = () => toast('Here is your toast.')
 
 const Index = () => {
-    // react-hook-form
     return (
-        <div className={styles.content}>
-            <CssBaseline />
-            <Container maxWidth="sm" classes={{ root: styles.container }}>
-
-                <div className={styles.actions}>
-                    <Button
-                        variant="contained"
-                        classes={{ root: styles.Button }}
-                    >
-                        トーストテスト
-                    </Button>
-                </div>
-            </Container>
+        <div className={styles.button}>
+            <button onClick={notify}>Make me a toast</button>
+            <Toaster />
         </div >
     )
 }
